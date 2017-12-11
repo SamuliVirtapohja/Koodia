6e0ac6a3dc30d188535380879451a1f46bf1e910
Gromathos <samuli.virtapohja@gmail.com>
2017-12-11 21:01:21 +0200

    Login korjaus

--- Projekti/Projekti/Kirjautuminen.h
+++ Projekti/Projekti/Kirjautuminen.h
@@ -3,9 +3,13 @@ struct kayttaja {
 	char salasana[20], nimi[20];
 }; // luodaan k‰ytt‰j‰ tietue
 
-
+// k‰ytt‰j‰n kanssa kohtaavat funktiot
 int Valikko(int); // valikon funktio
 void Aloitus(); // aloituksen funktio
+
+void asd(kayttaja);
+
+// funktiot mitk‰ eiv‰t koske k‰ytt‰j‰‰ mitenk‰‰n.
 void LuoKayttaja(); // k‰ytt‰j‰n luominen
 void Kirjautuminen(); // k‰ytt‰j‰n‰ kirjautuminen sis‰‰n
 
--- Projekti/Projekti/Valikko.cpp
+++ Projekti/Projekti/Valikko.cpp
@@ -14,7 +14,7 @@ void Aloitus() {// Käyttäjän syötön perusteella:
 
 	do // do/while looppi kirjautumismenuun ja käyttäjän luomiseen
 	{
-		kayttajansyotto = Valikko(kayttajansyotto); 
+		kayttajansyotto = Valikko(kayttajansyotto); // palautetaan käyttäjänsyöttö
 		switch (kayttajansyotto) {// käyttäjän syötön perusteella
 		case 1: {
 			LuoKayttaja(); // luodaan käyttäjä
@@ -47,31 +47,34 @@ void Kirjautuminen() { // funktio kirjautumiselle
 	ifstream tiedosto;
 	string rivi, tiedostonimi, tiedostosalasana;
 	kayttaja user;
+	bool tiedostonloppu = false;
+
 
 	cout << "Anna käyttäjänimi: "; // käyttäjältä kysytään nimi
 	cin >> ws >> user.nimi; // otetaan vain käyttäjän syöttö talteen
 	cout << "\nAnna salasana: "; // kysytään salasana
 	cin >> ws >> user.salasana; // otetaan vain käyttäjän syöttö talteen
-	tiedosto.open("kayttajat.txt"); // avataan käyttäjät.txt
 
+	tiedosto.open("kayttajat.txt"); // avataan käyttäjät.txt
 	if (tiedosto.is_open()) {// jos tiedosto avataan onnistuneesti
 		while (getline(tiedosto, rivi)) {// haetaan jokaiselta riviltä tiedoston loppuun saakka
-			if (tiedosto.eof())
-			{
-				cout << "Käyttäjätunnus tai salasana väärin!";
-				break;
-			}
 			tiedostonimi = rivi.substr(0, rivi.find(" ")); // haetaan jokaiselta riviltä ennen väliä (eli käyttäjiä etsitään) oleva sana
 			tiedostosalasana = rivi.substr(rivi.find(" ") + 1); // haetaan välin jälkeen (eli salasanoja etsitään) oleva sana
 			if (user.nimi == tiedostonimi && user.salasana == tiedostosalasana) { // jos käyttäjän syöttämä nimi ja salasana matchaa, kirjaudutaan sisään.
-				cout << "hienoa kirjauduit sisään" << endl; 
+				cout << "hienoa kirjauduit sisään" << endl;
+				tiedostonloppu = true; // asetetaan tiedostonloppu trueksi, niin ei tule virheilmoitusta väärästä käyttäjästä ja salasanasta
 			}
-		}
-		tiedosto.close(); // tiedosto suljetaan
+		}							
 	}
-	else{// jos jokin meni mönkään
+	else{// jos jokin meni mönkään tiedoston avaamisessa
 		cout << "tiedostoa ei voitu avata" << endl;
 	}
+
+	if (tiedostonloppu == false) // jos tiedostonloppu-boolean ei muutu annetaan virheilmoitus väärästä salasanasta ja käyttäjästä
+	{
+		cout << "Käyttäjätunnus tai salasana väärin!" << endl; // virheilmoitus
+	}
+
 	tiedosto.close();// suljetaan käyttäjät.txt
 }
 
